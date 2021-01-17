e46ae22c42be0e4fe4f2bcec2b96a00eb39d94ac
paucablop <paucablop@hotmail.com>
2021-01-17 22:56:54 +0100

    Update syntax

--- batch/mass_balance.py
+++ batch/mass_balance.py
@@ -20,9 +20,12 @@ def mass_balance(c, t):
     s[0, 0] = -1
     s[0, 1] = parameters.y_x_s
     
+    # Calculate rates
+    mu = kinetics.monod(parameters, c[0])
+    
     # Differential equations
-    dxdt = kinetics.monod(parameters, c[0]) * s[0, 0] * c[1] # Glucose
-    dydt = kinetics.monod(parameters, c[0]) * s[0, 1] * c[1] # Biomass
+    dxdt = mu * s[0, 0] * c[1] # Glucose
+    dydt = mu * s[0, 1] * c[1] # Biomass
     
     dzdt = [dxdt, dydt]
     return dzdt
